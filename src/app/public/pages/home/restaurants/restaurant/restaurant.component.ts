99ab7355e7a368f2ed47640a78c7120c7c60eeda
henao21 <edgar.henao@correounivalle.edu.co>
2022-03-15 19:42:30 -0500

    quintero nos jodio

--- src/app/public/pages/home/restaurants/restaurant/restaurant.component.ts
+++ src/app/public/pages/home/restaurants/restaurant/restaurant.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
   styleUrls: ['./restaurant.component.css']
 })
 export class RestaurantComponent implements OnInit {
+
+  Empresas:Array<any> = [
+    {nombre: 'El corral',
+    direccion: 'carrera 3 #3-42',
+    calificacion: '***'
+  }
+  ]
+
+  Menus:Array<any> = [
+    {titulo: 'Combo Whopper',
+    descripcion: 'Hamburguesa con pan con ajonjolí, carne de res artesanal 115 g, tajada cheddar, tomate, lechuga y cebolla grillé',
+    precio: '17.900,00'
+  },
+  {titulo: 'Combo Whopper',
+    descripcion: 'Hamburguesa con pan con ajonjolí, carne de res artesanal 115 g, tajada cheddar, tomate, lechuga y cebolla grillé',
+    precio: '17.900,00'
+  }
+  ]
 
   constructor() { }
 
--- src/app/public/pages/home/restaurants/restaurants.component.ts
+++ src/app/public/pages/home/restaurants/restaurants.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
+
+  Empresas:Array<any> = [
+    {tipo: 'Restaurantes',
+    nombre: 'El corral'}
+  ]
 
   constructor() { }
 
